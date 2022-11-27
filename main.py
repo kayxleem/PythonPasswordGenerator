50c66319d318acec4b0b0567868c87f0e1a5d350
Adegbuyi Oluwakayode <kayode_adegbuyi@yahoo.com>
2022-11-27 23:03:30 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+print("Welcome to the Password Generator")
+
+pw_letters = input("How many letters will you like in your password: ")
+
+pw_symbols = input("How many symbols will you like: ")
+
+pw_number = input("How many numbers will you like: ")
+
